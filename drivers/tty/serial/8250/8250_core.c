6625cc7cc3ace4cb18745107385c34f7b5dd1eb3
Karthik Manamcheri <karthik.manamcheri@ni.com>
2014-03-14 13:15:59 -0500

    8250: Make SERIAL_8250_RUNTIME_UARTS work correctly
    Consider a situation where I have an ARM based system (such as the
    Zynq) and therefore no legacy ports. I have three memory-mapped ports
    (such as cRIO-9068). I use device tree to describe the ports. What
    would be the config options I set so that I get only the three ports in
    my system? I do not want legacy ports being created automatically and I
    want it to be flexible enough that it creates the devices based only on
    the device tree. I expected setting SERIAL_8250_RUNTIME_UARTS = 0 to
    work because the description said, "Set this to the maximum number of
    serial ports you want the kernel to register at boot time."
    Unfortunately, even though SERIAL_8250_NR_UARTS was set to the default
    value of 4, I did not get any device nodes (because
    SERIAL_8250_RUNTIME_UARTS was 0). This is what this change is
    addressing.
    
    SERIAL_8250_NR_UARTS controls the maximum number of ports you can
    support. SERIAL_8250_RUNTIME_UARTS specifies the number of ports you
    want to create automatically for legacy ports at boot time. All other
    ports will be created when serial8250_register_port is called (and if
    it does not exceed the total number of supported ports as specified by
    SERIAL_8250_NR_UARTS).
    
    Signed-off-by: Karthik Manamcheri <karthik.manamcheri@ni.com>
    Acked-by: Jaeden Amero <jaeden.amero@ni.com>
    
    Revert "serial: 8250: Do nothing if nr_uarts=0"
    
    This reverts commit 59cfc45f17d6d1dda2990e6f5a94df24a18330b8. It
    conflicts with "8250: Make SERIAL_8250_RUNTIME_UARTS work correctly",
    and so prevents systems whose UARTs are all hotplugged from being able
    to set nr_uarts=0 on bootup.
    
    Natinst-CAR-ID: 634278
    Signed-off-by: Richard Tollerton <rich.tollerton@ni.com>
    Signed-off-by: Brad Mouring <brad.mouring@ni.com>
    Natinst-ReviewBoard-ID: 183619

--- drivers/tty/serial/8250/8250_core.c
+++ drivers/tty/serial/8250/8250_core.c
@@ -500,7 +500,7 @@ static void __init serial8250_isa_init_ports(void)
 	if (nr_uarts > UART_NR)
 		nr_uarts = UART_NR;
 
-	for (i = 0; i < nr_uarts; i++) {
+	for (i = 0; i < UART_NR; i++) {
 		struct uart_8250_port *up = &serial8250_ports[i];
 		struct uart_port *port = &up->port;
 
@@ -593,7 +593,7 @@ static int univ8250_console_setup(struct console *co, char *options)
 	 * if so, search for the first available port that does have
 	 * console support.
 	 */
-	if (co->index >= nr_uarts)
+	if (co->index >= UART_NR)
 		co->index = 0;
 	port = &serial8250_ports[co->index].port;
 	/* link port to console */
@@ -646,7 +646,7 @@ static int univ8250_console_match(struct console *co, char *name, int idx,
 		return -ENODEV;
 
 	/* try to match the port specified on the command line */
-	for (i = 0; i < nr_uarts; i++) {
+	for (i = 0; i < UART_NR; i++) {
 		struct uart_port *port = &serial8250_ports[i].port;
 
 		if (port->iotype != iotype)
@@ -680,9 +680,6 @@ static struct console univ8250_console = {
 
 static int __init univ8250_console_init(void)
 {
-	if (nr_uarts == 0)
-		return -ENODEV;
-
 	serial8250_isa_init_ports();
 	register_console(&univ8250_console);
 	return 0;
@@ -713,7 +710,7 @@ int __init early_serial_setup(struct uart_port *port)
 {
 	struct uart_port *p;
 
-	if (port->line >= ARRAY_SIZE(serial8250_ports) || nr_uarts == 0)
+	if (port->line >= ARRAY_SIZE(serial8250_ports))
 		return -ENODEV;
 
 	serial8250_isa_init_ports();
@@ -853,7 +850,7 @@ static int serial8250_remove(struct platform_device *dev)
 {
 	int i;
 
-	for (i = 0; i < nr_uarts; i++) {
+	for (i = 0; i < UART_NR; i++) {
 		struct uart_8250_port *up = &serial8250_ports[i];
 
 		if (up->port.dev == &dev->dev)
@@ -920,7 +917,7 @@ static struct uart_8250_port *serial8250_find_match_or_unused(const struct uart_
 	/*
 	 * First, find a port entry which matches.
 	 */
-	for (i = 0; i < nr_uarts; i++)
+	for (i = 0; i < UART_NR; i++)
 		if (uart_match_port(&serial8250_ports[i].port, port))
 			return &serial8250_ports[i];
 
@@ -934,7 +931,7 @@ static struct uart_8250_port *serial8250_find_match_or_unused(const struct uart_
 	 * free entry.  We look for one which hasn't been previously
 	 * used (indicated by zero iobase).
 	 */
-	for (i = 0; i < nr_uarts; i++)
+	for (i = 0; i < UART_NR; i++)
 		if (serial8250_ports[i].port.type == PORT_UNKNOWN &&
 		    serial8250_ports[i].port.iobase == 0)
 			return &serial8250_ports[i];
@@ -943,7 +940,7 @@ static struct uart_8250_port *serial8250_find_match_or_unused(const struct uart_
 	 * That also failed.  Last resort is to find any entry which
 	 * doesn't have a real port associated with it.
 	 */
-	for (i = 0; i < nr_uarts; i++)
+	for (i = 0; i < UART_NR; i++)
 		if (serial8250_ports[i].port.type == PORT_UNKNOWN)
 			return &serial8250_ports[i];
 
@@ -1171,9 +1168,6 @@ EXPORT_SYMBOL(serial8250_unregister_port);
 static int __init serial8250_init(void)
 {
 	int ret;
-
-	if (nr_uarts == 0)
-		return -ENODEV;
 
 	serial8250_isa_init_ports();
 
