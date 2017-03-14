316cd4422ec32256105e85b1d2328b39e6cbc63d
Wan Ahmad Zainie <wan.ahmad.zainie.wan.mohamad@intel.com>
2017-03-14 15:45:21 -0500

    usb: dwc3: call _DSM for core soft reset
    The issue is, if core soft reset is issued while Intel Apollo Lake
    USB mux is in Host role mode, it takes close to 7 minutes before we
    are able to switch USB mux from Host mode to Device mode. This is
    due to RTL bug.
    
    The workaround is to let BIOS issue the core soft reset via _DSM
    method. It will ensure that USB mux is in Device role mode before
    issuing core soft reset, and will inform the driver whether the
    reset is success within the timeout value, or the timeout is exceeded.
    
    commit cd78b8067c6e ("usb: dwc3: call _DSM for core soft reset")
    originated from http://git.yoctoproject.org/cgit/cgit.cgi/linux-yocto-4.1/
    
    Signed-off-by: Wan Ahmad Zainie <wan.ahmad.zainie.wan.mohamad@intel.com>
    [akash.mankar@ni.com: changed the way has_dsm_for_softreset property is
    set in dwc3-pci.c and read in core.c]
    Signed-off-by: Akash Mankar <akash.mankar@ni.com>
    Signed-off-by: Brad Mouring <brad.mouring@ni.com>
    Acked-by: Gratian Crisan <gratian.crisan@ni.com>
    Acked-by: Brandon Streiff <brandon.streif@ni.com>
    Natinst-ReviewBoard-ID: 178124
    [gratian: fixed merge conflicts, mainly due to dwc3_soft_reset removal]
    Signed-off-by: Gratian Crisan <gratian.crisan@ni.com>
    [bstreiff: fixed merge conflicts due to property refactor by 1a7b12f69a94
     ("usb: dwc3: pci: Supply device properties via driver data")]
    [gratian: fix merge conflict with f580170f135a
     ("usb: dwc3: Add splitdisable quirk for Hisilicon Kirin Soc")]
    Signed-off-by: Gratian Crisan <gratian.crisan@ni.com>

--- drivers/usb/dwc3/dwc3-pci.c
+++ drivers/usb/dwc3/dwc3-pci.c
@@ -122,6 +122,13 @@ static const struct property_entry dwc3_pci_intel_properties[] = {
 	{}
 };
 
+static const struct property_entry dwc3_pci_intel_bxt_properties[] = {
+	PROPERTY_ENTRY_STRING("dr_mode", "peripheral"),
+	PROPERTY_ENTRY_BOOL("snps,has_dsm_for_softreset"),
+	PROPERTY_ENTRY_BOOL("linux,sysdev_is_parent"),
+	{}
+};
+
 static const struct property_entry dwc3_pci_intel_byt_properties[] = {
 	PROPERTY_ENTRY_STRING("dr_mode", "peripheral"),
 	PROPERTY_ENTRY_BOOL("snps,dis_u2_susphy_quirk"),
@@ -169,6 +176,10 @@ static const struct property_entry dwc3_pci_mr_properties[] = {
 
 static const struct software_node dwc3_pci_intel_swnode = {
 	.properties = dwc3_pci_intel_properties,
+};
+
+static const struct software_node dwc3_pci_intel_bxt_swnode = {
+	.properties = dwc3_pci_intel_bxt_properties,
 };
 
 static const struct software_node dwc3_pci_intel_byt_swnode = {
@@ -373,13 +384,13 @@ static const struct pci_device_id dwc3_pci_id_table[] = {
 	  (kernel_ulong_t) &dwc3_pci_intel_swnode, },
 
 	{ PCI_VDEVICE(INTEL, PCI_DEVICE_ID_INTEL_BXT),
-	  (kernel_ulong_t) &dwc3_pci_intel_swnode, },
+	  (kernel_ulong_t) &dwc3_pci_intel_bxt_swnode, },
 
 	{ PCI_VDEVICE(INTEL, PCI_DEVICE_ID_INTEL_BXT_M),
-	  (kernel_ulong_t) &dwc3_pci_intel_swnode, },
+	  (kernel_ulong_t) &dwc3_pci_intel_bxt_swnode, },
 
 	{ PCI_VDEVICE(INTEL, PCI_DEVICE_ID_INTEL_APL),
-	  (kernel_ulong_t) &dwc3_pci_intel_swnode, },
+	  (kernel_ulong_t) &dwc3_pci_intel_bxt_swnode, },
 
 	{ PCI_VDEVICE(INTEL, PCI_DEVICE_ID_INTEL_KBP),
 	  (kernel_ulong_t) &dwc3_pci_intel_swnode, },
