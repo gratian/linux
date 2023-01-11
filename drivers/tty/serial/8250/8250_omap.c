8cbe9110c62514c0f4b1290cae1b216f06b5b8e6
Gratian Crisan <gratian.crisan@ni.com>
2023-01-11 16:54:42 -0600

    Revert "Revert "serial: Deassert Transmit Enable on probe in driver-specific way""
    This reverts commit 9ce2640de515dfa5d4542de1883075a5ff535b66.
    
    This re-introduces the upstream linux-stable changes and makes future
    upgrades/merges easier.
    
    Work has been scheduled to fix the affected RS-485 serial ATS tests which
    incorrectly configured the RS-485 flags and caused the failures that
    prompted the original revert.
    
    Signed-off-by: Gratian Crisan <gratian.crisan@ni.com>

--- drivers/tty/serial/8250/8250_omap.c
+++ drivers/tty/serial/8250/8250_omap.c
@@ -344,7 +344,10 @@ static void omap8250_restore_regs(struct uart_8250_port *up)
 
 	omap8250_update_mdr1(up, priv);
 
-	up->port.ops->set_mctrl(&up->port, up->port.mctrl);
+	__omap8250_set_mctrl(&up->port, up->port.mctrl);
+
+	if (up->port.rs485.flags & SER_RS485_ENABLED)
+		serial8250_em485_stop_tx(up);
 }
 
 /*
